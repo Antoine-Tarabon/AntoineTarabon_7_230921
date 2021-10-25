671589aaa7fb110097d00a951046580fc0ff7063
Antoine-Tarabon <pandore4@outlook.fr>
2021-10-25 17:00:14 +0200

    ajout du form se connecter et debut du form sinscrire

--- frontend/src/pages/Profil.js
+++ frontend/src/pages/Profil.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Log from '../components/log';
 
 const Profil = () => {
     return (
-        <div>
-            page profil
+        <div className="profil-page">
+            <div className="log-container">
+                <Log signin={false} signup={true} />
+            </div>
         </div>
     );
 };
