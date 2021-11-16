d72ed2f171711c6384363d9a6a97655316473ec8
Antoine-Tarabon <pandore4@outlook.fr>
2021-11-16 12:25:48 +0100

    likebutton like unlike redux

--- frontend/src/components/Post/Card.js
+++ frontend/src/components/Post/Card.js
@@ -1,26 +1,85 @@
-import React, {useEffect, useState} from 'react';
-import { useSelector } from 'react-redux'
-import { isEmpty } from '../Utils';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { dateParser, isEmpty } from "../Utils";
+import FollowHandler from "../Profil/FollowHandler";
+import LikeButton from "./LikeButton";
+//import { updatePost } from "../../actions/post.action";
+//import DeleteCard from "./DeleteCard";
+//import CardComments from "./CardComments";
 
-const Card = (post) => {
-    const [isLoading, setIsLoading] = useState(true);
-    const usersData = useSelector((state) => state.usersReducer);
-    const userData = useSelector((state) => state.userReducer);
-
-    useEffect(() => {
-        !isEmpty(userData[0]) && setIsLoading(false);
-    })
+const Card = ({ post }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  //const [isUpdated, setIsUpdated] = useState(false);
+  //const [showComments, setShowComments] = useState(false);
+  const usersData = useSelector((state) => state.usersReducer);
+  const userData = useSelector((state) => state.userReducer);
 
 
-    return (
-        <li className="card-container" key={post._id}>
-            {isLoading ? (
-                <i className="fas fa-spinner fa-spin"></i>
-            ) : (
-                <h2>test</h2>
+  useEffect(() => {
+    !isEmpty(usersData[0]) && setIsLoading(false);
+  }, [usersData]);
+
+  return (
+    <li className="card-container" key={post._id}>
+      {isLoading ? (
+        <i className="fas fa-spinner fa-spin"></i>
+      ) : (
+        <>
+          <div className="card-left">
+            <img
+              src={
+                !isEmpty(usersData[0]) &&
+                usersData
+                  .map((user) => {
+                    if (user._id === post.posterId) return user.picture;
+                    else return null;
+                  })
+                  .join("")
+              }
+              alt="poster-pic"
+            />
+          </div>
+          <div className="card-right">
+            <div className="card-header">
+              <div className="pseudo">
+                <h3>
+                  {!isEmpty(usersData[0]) &&
+                    usersData
+                      .map((user) => {
+                        if (user._id === post.posterId) return user.pseudo;
+                        else return null;
+                      })
+                      .join("")}
+                </h3>
+                {post.posterId !== userData._id && (
+                  <FollowHandler idToFollow={post.posterId} type={"card"} />
+                )}
+              </div>
+              <span>{dateParser(post.createdAt)}</span>
+            </div>
+            {post.picture && (
+              <img src={post.picture} alt="card-pic" className="card-pic" />
             )}
-        </li>
-    );
+            {post.video && (
+              <iframe
+                width="500"
+                height="300"
+                src={post.video}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title={post._id}
+              ></iframe>
+            )}
+            <div className="card-footer">
+              <LikeButton post={post} />
+            </div>
+            <h3>CardComments post={post}</h3> 
+          </div>
+        </>
+      )}
+    </li>
+  );
 };
 
 export default Card;
